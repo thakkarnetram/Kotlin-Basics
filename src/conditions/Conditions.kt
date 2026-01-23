84d2dc1f96215c64e76698779bfca8cfc2c3d549
thakkarnetram <thakkarnetram10@gmail.com>
2026-01-23 14:17:25 +0530

    more explaination on when statements

--- src/conditions/Conditions.kt
+++ src/conditions/Conditions.kt
@@ -1,12 +1,18 @@
 package conditions
 
 fun main(){
+    /* when statements can be used in 3 cases
+    * use when as an expression
+    * use when with a value
+    * use when with a value as an expression
+    * */
     val password = readlnOrNull()
     // can be used this way as well
     val adminPassword = when {
         password == "admin@123" -> "Admin Access granted"
          else -> "Basic access granted"
     }
+    // this is when as an expression
     val hasAccess = when {
         password?.contains("superadmin") == true -> {
             true
@@ -17,7 +23,8 @@ fun main(){
         true -> println("Super Admin Access Granted ")
         else -> println("Basic Access Granted")
     }
-    // another way to write conditions using when
+    // another way to write conditions using when.
+    // when with a value
     when(password) {
         "admin@123" -> println("Full access to database")
         "admin@12345" -> println("Full access to server control")
@@ -36,4 +43,19 @@ fun main(){
         }
     }
     println(adminPassword)
+    findProbability()
+}
+
+// another example usage of when
+fun findProbability() {
+    // ? indicates null safety checks
+    val probability : Int? = readlnOrNull()?.toInt();
+    if (probability != null) {
+        when {
+            probability < 40 -> println("Less Chances")
+            probability <= 80 -> println("More Chances")
+            probability < 100 -> println("100% Chances")
+            else -> println("No Idea")
+        }
+    }
 }
