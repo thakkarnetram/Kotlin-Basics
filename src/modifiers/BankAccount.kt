50dada7b6447c10fe11c89f70898b4fff03d38af
thakkarnetram <thakkarnetram10@gmail.com>
2026-02-12 14:47:02 +0530

    internal and protected modifiers

--- src/modifiers/BankAccount.kt
+++ src/modifiers/BankAccount.kt
@@ -1,28 +1,36 @@
 package modifiers
 
 open class BankAccount (
-    private var accountBalance : Int
+    private var accountBalance : Int,
+    // protected is only visible to its own and child classes
+    protected var bankName : String
 ) {
     // making this private so other classes cannot access it
     // we don't want other class making changes to this
     // making a val/var/methods/interfaces/fun/class open allows other files and classes to get access of it.
+    // INTERNAL modifier is used when we ship a module or library and only the library can use that method or class
+    // example - If I have a library / module for getting weather forecast and I use it inside my application , if any method,var,class is marked as internal in
+    // the library it can't be used inside my application
     private var canWithdraw : Boolean = false
     fun depositAmount(amountToDeposit : Int) {
         accountBalance+=amountToDeposit
-        println("Updated balance in your account is $accountBalance")
+        println("Amount deposit success ! Updated balance in your account is $accountBalance")
     }
     fun withdrawAmount(amount : Int) {
         if(accountBalance >= amount) {
             canWithdraw = true
             accountBalance -= amount;
-            println("$amount is in process for withdrawal")
+            println("$amount is in process for withdrawal and your updated balance is $accountBalance")
         } else {
             canWithdraw = false
-            println("Withdrawal of $amount can't be processed due to insufficient balance")
+            println("Withdrawal of $amount can't be processed due to insufficient balance $accountBalance")
         }
     }
     fun checkBalance () {
         println("Your current balance is $accountBalance")
+    }
+    fun getBankName() {
+        println("Your Bank Account is in $bankName and has a balance of $accountBalance")
     }
 }
 
--- src/modifiers/Modifiers.kt
+++ src/modifiers/Modifiers.kt
@@ -2,10 +2,11 @@ package modifiers
 
 fun main(){
     // here I cant access bankAccount class if its private but if I make it open I can access
-    val person = BankAccount(50000)
+    val person = BankAccount(50000,"Kotak Mahindra Bank")
     // I can't access this variable as its private and ofc we don't want this variable to be accessed by someone to tamper the logic
 //    person.canWithdraw = true
     person.checkBalance()
+    person.getBankName()
     person.withdrawAmount(4000)
     person.depositAmount(3000)
 }
